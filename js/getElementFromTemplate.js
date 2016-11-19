bef6599cc20984ab237f9c40193f4c207a61c2a2
Vadim Ivanov <ivanov@morizo.ru>
2016-11-19 13:28:36 +0300

    Шаблоны перенесены в js

--- js/getElementFromTemplate.js
+++ js/getElementFromTemplate.js
@@ -1,6 +1,5 @@
 export default (template) => {
   let elem = document.createElement('div');
   elem.innerHTML = template;
-
-  return elem.children[0];
+  return elem;
 };
---
+++ js/templates/intro.js
@@ -0,0 +1,12 @@
+import getElementFromTemplate from './getElementFromTemplate';
+
+const template =
+  `<div id="intro" class="intro">
+    <h1 class="intro__asterisk">*</h1>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf
+      Sparnaay.
+    </p>
+  </div>`;
+
+const templateElem = getElementFromTemplate(template);
+export default templateElem;
