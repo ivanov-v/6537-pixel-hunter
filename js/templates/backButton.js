a6ef56e9e2535659db04fd656f61d46c326fd9e2
Vadim Ivanov <ivanov@morizo.ru>
2016-11-20 21:22:08 +0300

    Вынесены дополнительные шаблоны

---
+++ js/templates/backButton.js
@@ -0,0 +1,9 @@
+const backButton =
+`<div class="header__back">
+  <span class="back">
+    <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
+    <img src="img/logo_small.png" width="101" height="44">
+  </span>
+</div>`;
+
+export default backButton;
