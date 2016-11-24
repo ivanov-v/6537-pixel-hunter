264e8368259199f51f6a048496ba2852b2012440
Vadim Ivanov <sferamail33@yandex.ru>
2016-11-25 00:09:57 +0300

    Добавлены данные для intro

--- js/pages/intro.js
+++ js/pages/intro.js
@@ -2,12 +2,14 @@ import getElement from '../getElement';
 import renderPage from '../renderPage';
 import greeting from './greeting';
 
+const intro = {
+  motto: '<sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.'
+};
+
 const template =
   `<div id="intro" class="intro">
     <h1 class="intro__asterisk">*</h1>
-    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf
-      Sparnaay.
-    </p>
+    <p class="intro__motto">${intro.motto}</p>
   </div>`;
 
 const templateElem = getElement(template);
