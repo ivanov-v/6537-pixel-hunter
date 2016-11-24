f8531715eafe6c95a6df720dd7afcc18b0661399
Vadim Ivanov <sferamail33@yandex.ru>
2016-11-25 00:20:44 +0300

    Добавлены данные для rules

--- js/pages/rules.js
+++ js/pages/rules.js
@@ -3,22 +3,26 @@ import renderPage from '../renderPage';
 import backButton from '../templates/backButton';
 import game1 from './game-1';
 
+const rules = {
+  title: 'Правила',
+  description: `Угадай 10 раз для каждого изображения фото <img
+    src="img/photo_icon.png" width="16" height="16"> или рисунок <img
+    src="img/paint_icon.png" width="16" height="16" alt="">.<br>
+    Фотографиями или рисунками могут быть оба изображения.<br>
+    На каждую попытку отводится 30 секунд.<br>
+    Ошибиться можно не более 3 раз.<br>
+    <br>
+    Готовы?`
+};
+
 const template =
   `<div id="rules">
     <header class="header">
       ${backButton}
     </header>
     <div class="rules  central--none">
-      <h1 class="rules__title">Правила</h1>
-      <p class="rules__description">Угадай 10 раз для каждого изображения фото <img
-        src="img/photo_icon.png" width="16" height="16"> или рисунок <img
-        src="img/paint_icon.png" width="16" height="16" alt="">.<br>
-        Фотографиями или рисунками могут быть оба изображения.<br>
-        На каждую попытку отводится 30 секунд.<br>
-        Ошибиться можно не более 3 раз.<br>
-        <br>
-        Готовы?
-      </p>
+      <h1 class="rules__title">${rules.title}</h1>
+      <p class="rules__description">${rules.description}</p>
       <form class="rules__form">
         <input class="rules__input" type="text" placeholder="Ваше Имя">
         <button class="rules__button  continue" type="submit" disabled>Go!</button>
