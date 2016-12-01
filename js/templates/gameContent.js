10c3c64a72467ea0190a7cbe1779b52b2526edd5
Vadim Ivanov <sferamail33@yandex.ru>
2016-12-01 23:08:40 +0300

    Добавлена шаблонизация gameContent

--- js/templates/gameContent.js
+++ js/templates/gameContent.js
@@ -1,3 +1,55 @@
-export default () => {
+import gameAnswers from '../templates/gameAnswers';
 
+const settingsVariants = [
+  {
+    contentType: 'wide',
+    imageSizes: {
+      width: 705,
+      height: 455
+    }
+  },
+  {
+    contentType: null,
+    imageSizes: {
+      width: 468,
+      height: 458
+    }
+  },
+  {
+    contentType: 'triple',
+    imageSizes: {
+      width: 304,
+      height: 455
+    }
+  }
+];
+
+const getSettings = (settings, optionsLength) => {
+  switch (optionsLength) {
+    case 1:
+      return settings[0];
+    case 2:
+      return settings[1];
+    case 3:
+      return settings[2];
+    default:
+      return settings[1];
+  }
+};
+
+export default (options) => {
+  const selectedSetings = getSettings(settingsVariants, options.length);
+
+  const {contentType: contentType, imageSizes: {width, height}} = selectedSetings;
+
+  const optionsTemplate = options.map((option, index) => {
+    return `
+      <div class="game__option">
+        <img src="${option.image}" alt="Option ${index + 1}" width="${width}" height="${height}">
+        ${option.answers ? gameAnswers(option.answers) : ''}
+      </div>
+    `;
+  });
+
+  return `<form class="game__content ${contentType ? `game__content--${contentType}` : ''}">${optionsTemplate.join('')}</form>`;
 };
