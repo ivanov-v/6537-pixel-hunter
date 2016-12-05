4b636672b3c50fccf1d6b94f02b7ee19c2ffe189
Vadim Ivanov <sferamail33@yandex.ru>
2016-12-05 22:41:06 +0300

    Изменен модуль rules

--- js/pages/rules.js
+++ js/pages/rules.js
@@ -1,54 +1,42 @@
 import getElement from '../getElement';
 import renderPage from '../renderPage';
-import backButton from '../templates/backButton';
+import header from '../templates/header';
 import game1 from './game-1';
 
-const rules = {
-  title: 'Правила',
-  description: `Угадай 10 раз для каждого изображения фото <img
-    src="img/photo_icon.png" width="16" height="16"> или рисунок <img
-    src="img/paint_icon.png" width="16" height="16" alt="">.<br>
-    Фотографиями или рисунками могут быть оба изображения.<br>
-    На каждую попытку отводится 30 секунд.<br>
-    Ошибиться можно не более 3 раз.<br>
-    <br>
-    Готовы?`
+export default (data) => {
+  const template =
+    `<div>
+      ${header()}
+      <div class="rules  central--none">
+        <h1 class="rules__title">${data.title}</h1>
+        <p class="rules__description">${data.description}</p>
+        <form class="rules__form">
+          <input class="rules__input" type="text" placeholder="Ваше Имя">
+          <button class="rules__button  continue" type="submit" disabled>Go!</button>
+        </form>
+      </div>
+    </div>`;
+
+  const element = getElement(template);
+  const form = element.querySelector('.rules__form');
+  const input = form.querySelector('.rules__input');
+  const goButton = form.querySelector('.rules__button.continue');
+
+  const inputHandler = (evt) => {
+    if (evt.target.value) {
+      goButton.disabled = false;
+    } else {
+      goButton.disabled = true;
+    }
+  };
+
+  const formHandler = (evt) => {
+    evt.preventDefault();
+    renderPage(game1);
+  };
+
+  form.addEventListener('submit', formHandler);
+  input.addEventListener('input', inputHandler);
+
+  return element;
 };
-
-const template =
-  `<div id="rules">
-    <header class="header">
-      ${backButton}
-    </header>
-    <div class="rules  central--none">
-      <h1 class="rules__title">${rules.title}</h1>
-      <p class="rules__description">${rules.description}</p>
-      <form class="rules__form">
-        <input class="rules__input" type="text" placeholder="Ваше Имя">
-        <button class="rules__button  continue" type="submit" disabled>Go!</button>
-      </form>
-    </div>
-  </div>`;
-
-const templateElem = getElement(template);
-export default templateElem;
-
-const form = templateElem.querySelector('.rules__form');
-const input = form.querySelector('.rules__input');
-const goButton = form.querySelector('.rules__button.continue');
-
-const inputHandler = (evt) => {
-  if (evt.target.value) {
-    goButton.disabled = false;
-  } else {
-    goButton.disabled = true;
-  }
-};
-
-const formHandler = (evt) => {
-  evt.preventDefault();
-  renderPage(game1);
-};
-
-form.addEventListener('submit', formHandler);
-input.addEventListener('input', inputHandler);
