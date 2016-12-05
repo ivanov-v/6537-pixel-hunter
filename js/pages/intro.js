7638148171415a72f45471fd94704a114b1a405d
Vadim Ivanov <sferamail33@yandex.ru>
2016-12-05 22:33:24 +0300

    Изменен модуль intro

--- js/pages/intro.js
+++ js/pages/intro.js
@@ -2,21 +2,16 @@ import getElement from '../getElement';
 import renderPage from '../renderPage';
 import greeting from './greeting';
 
-const intro = {
-  motto: '<sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.'
+export default (data) => {
+  const template =
+    `<div id="intro" class="intro">
+      <h1 class="intro__asterisk">*</h1>
+      <p class="intro__motto">${data.motto}</p>
+    </div>`;
+
+  const element = getElement(template);
+  const asterisk = element.querySelector('.intro__asterisk');
+  asterisk.addEventListener('click', () => renderPage(greeting));
+
+  return element;
 };
-
-const template =
-  `<div id="intro" class="intro">
-    <h1 class="intro__asterisk">*</h1>
-    <p class="intro__motto">${intro.motto}</p>
-  </div>`;
-
-const templateElem = getElement(template);
-export default templateElem;
-
-const asterisk = templateElem.querySelector('.intro__asterisk');
-
-const asteriskHandler = (evt) => renderPage(greeting);
-
-asterisk.addEventListener('click', asteriskHandler);
