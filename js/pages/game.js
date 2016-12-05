d71d3f61fa9da4b3a0261e76040b66309bf31ce5
Vadim Ivanov <sferamail33@yandex.ru>
2016-12-05 22:06:12 +0300

    Создан модуль game для всех игр

---
+++ js/pages/game.js
@@ -0,0 +1,21 @@
+import getElement from '../getElement';
+import header from '../templates/header';
+import gameTask from '../templates/gameTask';
+import gameStats from '../templates/gameStats';
+import gameContent from '../templates/gameContent';
+
+export default (data) => {
+  const gameTemplate =
+    `<div>
+      ${header(data.statusbar)}
+      <div class="game">
+        ${gameTask(data.question.description)}
+        ${gameContent(data)}
+        <div class="stats">
+          ${gameStats(data.stats)}
+        </div>
+      </div>
+    </div>`;
+
+  return getElement(gameTemplate);
+};
