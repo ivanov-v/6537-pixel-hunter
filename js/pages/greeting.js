af5a3e36722666433e22e43ca507a63cff008e72
Vadim Ivanov <sferamail33@yandex.ru>
2016-12-08 22:01:01 +0300

    Статические данные убраны в свои шаблоны

--- js/pages/greeting.js
+++ js/pages/greeting.js
@@ -2,21 +2,26 @@ import getElement from '../getElement';
 import renderPage from '../renderPage';
 import rules from './rules';
 
-export default (data) => {
-  const template =
-    `<div class="greeting  central--blur">
-      <div class="greeting__logo"><img src="img/logo_big.png" width="201" height="89" alt="Pixel Hunter"></div>
-      <h1 class="greeting__asterisk">*</h1>
-      <div class="greeting__challenge">
-        <h3>${data.title}</h3>
-        <p>${data.description}</p>
-      </div>
-      <div class="greeting__continue"><span><img src="img/arrow_right.svg" width="64" height="64" alt="Next"></span></div>
-    </div>`;
+const template =
+  `<div class="greeting  central--blur">
+    <div class="greeting__logo"><img src="img/logo_big.png" width="201" height="89" alt="Pixel Hunter"></div>
+    <h1 class="greeting__asterisk">*</h1>
+    <div class="greeting__challenge">
+      <h3>Лучшие художники-фотореалисты бросают&nbsp;тебе&nbsp;вызов!</h3>
+      <p>Правила игры просты.
+        <br>
+        Нужно отличить рисунок&nbsp;от фотографии и сделать выбор.<br>
+        Задача кажется тривиальной, но не думай, что все так просто.<br>
+        Фотореализм обманчив и коварен.
+        <br>
+        Помни, главное — смотреть очень внимательно.
+      </p>
+    </div>
+    <div class="greeting__continue"><span><img src="img/arrow_right.svg" width="64" height="64" alt="Next"></span></div>
+  </div>`;
 
-  const element = getElement(template);
-  const continueButton = element.querySelector('.greeting__continue');
-  continueButton.addEventListener('click', () => renderPage(rules));
+const element = getElement(template);
+const continueButton = element.querySelector('.greeting__continue');
+continueButton.addEventListener('click', () => renderPage(rules));
 
-  return element;
-};
+export default element;
--- js/pages/intro.js
+++ js/pages/intro.js
@@ -2,16 +2,14 @@ import getElement from '../getElement';
 import renderPage from '../renderPage';
 import greeting from './greeting';
 
-export default (data) => {
-  const template =
-    `<div id="intro" class="intro">
-      <h1 class="intro__asterisk">*</h1>
-      <p class="intro__motto">${data.motto}</p>
-    </div>`;
+const template =
+  `<div id="intro" class="intro">
+    <h1 class="intro__asterisk">*</h1>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+  </div>`;
 
-  const element = getElement(template);
-  const asterisk = element.querySelector('.intro__asterisk');
-  asterisk.addEventListener('click', () => renderPage(greeting));
+const element = getElement(template);
+const asterisk = element.querySelector('.intro__asterisk');
+asterisk.addEventListener('click', () => renderPage(greeting));
 
-  return element;
-};
+export default element;
